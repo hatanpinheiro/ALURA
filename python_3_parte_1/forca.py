3ca65c8ebdd2b0b19f04c8b59bad27f0267c05fe
Hatan Pinheiro Silva <53164475+hatanpinheiro@users.noreply.github.com>
2020-08-02 04:00:13 -0300

    Add files via upload

--- python_3_parte_1/forca.py
+++ python_3_parte_1/forca.py
@@ -3,6 +3,29 @@ def jogar():
     print("*********Jogo da Forca**********")
     print("********************************")
 
+    palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
+
+    enforcou = False
+    acertou = False
+
+    print(letras_acertadas)
+
+    #Enquanto True E True
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+            
+            index = index + 1
+        
+        print("jogando ...")
+
     print("fim do jogo!")
 
 if(__name__ == "__main__"):
