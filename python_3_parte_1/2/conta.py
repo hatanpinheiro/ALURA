65144bc8c18778853e6eb61fa99de8f2a1d7bfb1
Hatan Pinheiro Silva <53164475+hatanpinheiro@users.noreply.github.com>
2020-08-06 02:49:37 -0300

    Add files via upload

--- python_3_parte_1/2/conta.py
+++ python_3_parte_1/2/conta.py
@@ -2,7 +2,25 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo {} do títular {}".format(self.__saldo, self.__titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+
+# from conta import Conta
+# conta = Conta(123, "Nico", 55.5, 1000.0)
+# conta2 = Conta(321, "Marco", 100.0, 1000.0)
