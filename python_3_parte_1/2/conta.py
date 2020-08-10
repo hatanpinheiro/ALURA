82179e3577945e10cf08642f55a074b2ab64d488
Hatan Pinheiro Silva <53164475+hatanpinheiro@users.noreply.github.com>
2020-08-09 22:40:08 -0300

    Add files via upload

--- python_3_parte_1/2/conta.py
+++ python_3_parte_1/2/conta.py
@@ -1,38 +1,55 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        print("Construindo objeto...{}".format(self))
+        print("Construindo objeto ... {}".format(self))
         self.__numero = numero
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
 
     def extrato(self):
-        print("Saldo {} do títular {}".format(self.__saldo, self.__titular))
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
 
-    def get_saldo(self):
+    @property
+    def saldo(self):
         return self.__saldo
 
+    @property
     def get_titular(self):
         return self.__titular
 
     @property
-    def get_limite(self):
+    def limite(self):
         return self.__limite
 
-    @limite.setter   
-    def __limite(self, limite):
+    @limite.setter
+    def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
 
 # from conta import Conta
 # conta = Conta(123, "Nico", 55.5, 1000.0)
